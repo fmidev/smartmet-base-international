1ce0b7e19e02d8f37f9efcd9b93274bafafd4171
Mikko Rauhala <mikko@rauhala.net>
2019-10-30 15:11:57 +0200

    Add unixtools

---
+++ unixtools/smarmet-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+DATE=`date -u +%Y%m%d`
+
+#tar zcvf /smartmet/editor/backup/smartmet-server-antigua.tgz  --exclude='*.sqd'  --exclude='*.zip'  --exclude='*.tgz' --exclude-backups /smartmet
+
